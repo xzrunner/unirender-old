aad46b8d758101811846d86d78f0355a4fb6b061
yangkaibo <yangkaiboo@163.com>
2017-03-09 20:43:01 +0800

    fix build ios

--- external/ejoy2d/opengl.h
+++ external/ejoy2d/opengl.h
@@ -1,29 +1,26 @@
 #ifndef ejoy2d_opengl_h
 #define ejoy2d_opengl_h
 
-#if defined(__APPLE__) && defined(__MACH__)
-
-#define OPENGLES 3
-#include <OpenGL/gl3.h>
-#include <OpenGL/glext.h>
-
-#elif defined(__APPLE__)
-
-#define OPENGLES 2
-#include <OpenGLES/ES2/gl.h>
-#import <OpenGLES/ES2/glext.h>
-
+#if defined __APPLE__
+    #include "TargetConditionals.h"
+    #if TARGET_OS_IPHONE
+        #define OPENGLES 2
+        #include <OpenGLES/ES2/gl.h>
+        #import <OpenGLES/ES2/glext.h>
+    #elif TARGET_OS_MAC
+        #define OPENGLES 3
+        #include <OpenGL/gl3.h>
+        #include <OpenGL/glext.h>
+    #else
+        #   error "Unknown Apple platform"
+    #endif
 #elif defined(linux) || defined(__linux) || defined(__linux__)
-
-#define OPENGLES 2
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-
+    #define OPENGLES 2
+    #include <GLES2/gl2.h>
+    #include <GLES2/gl2ext.h>
 #else
-
-#define OPENGLES 0
-#include <GL/glew.h>
-
+    #define OPENGLES 0
+    #include <GL/glew.h>
 #endif
 
 #endif
