c42fedf735d73beb3cd4de36cc3a4c8f7f7bb23c
yangkaibo <yangkaiboo@163.com>
2017-03-11 17:30:04 +0800

    android build

---
+++ Android.mk
@@ -0,0 +1,27 @@
+INNER_SAVED_LOCAL_PATH := $(LOCAL_PATH)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := unirender
+LOCAL_CFLAGS := -std=gnu99
+
+LOCAL_C_INCLUDES  := \
+	${UNIRENDER_SRC_PATH}/include \
+	${UNIRENDER_SRC_PATH}/include/unirender \
+	${UNIRENDER_SRC_PATH}/external \
+	${LOGGER_SRC_PATH} \
+	${CU_SRC_PATH} \
+	
+
+LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.cpp" -print)) \
+	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.c" -print)) \
+
+LOCAL_STATIC_LIBRARIES := \
+	logger \
+
+include $(BUILD_STATIC_LIBRARY)	
+
+LOCAL_PATH := $(INNER_SAVED_LOCAL_PATH)
