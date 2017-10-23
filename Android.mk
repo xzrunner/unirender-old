a8481c628054ee84bf7de41c8797c2f7084fe484
zz <zhuguang@ejoy.com>
2017-10-23 16:46:21 +0800

    up makefile

--- Android.mk
+++ Android.mk
@@ -5,15 +5,16 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := unirender
-LOCAL_CFLAGS := -std=gnu99
+
+LOCAL_CFLAGS += -std=gnu99
 
 LOCAL_C_INCLUDES  := \
+	${CLIB_PATH} \
 	${UNIRENDER_SRC_PATH}/include \
 	${UNIRENDER_SRC_PATH}/include/unirender \
 	${UNIRENDER_SRC_PATH}/external \
 	${LOGGER_SRC_PATH} \
-	${CU_SRC_PATH} \
-	
+	${MEMMGR_SRC_PATH}/include \
 
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.cpp" -print)) \
