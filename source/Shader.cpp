72233df21fe5e31e55127861d90cb67162ec9b94
zz <zhuguang@ejoy.com>
2019-04-14 09:14:08 +0800

    [FIXED] binded vert layout

--- source/Shader.cpp
+++ source/Shader.cpp
@@ -15,8 +15,12 @@ Shader::Shader(RenderContext* rc, const char* vs, const char* fs,
 	, m_shader_id(-1)
 	, m_vert_layout_id(-1)
 {
+    auto old_id = rc->GetVertexLayout();
+
 	m_vert_layout_id = rc->CreateVertexLayout(va_list);
 	m_shader_id = rc->CreateShader(vs, fs, textures, no_header);
+
+    rc->BindVertexLayout(old_id);
 }
 
 Shader::~Shader()
